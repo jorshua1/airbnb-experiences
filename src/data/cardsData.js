abc2951d486bba83131c7454c83ce5d7b0318b2e
Jorshua Hernandez <jorshua321@gmail.com>
2022-02-26 21:56:23 -0500

    refactorizacion del proyecto

---
+++ src/data/cardsData.js
@@ -0,0 +1,38 @@
+export default [
+    {
+    imagen:"/Assets/image 12.png",
+    estado:"Vendido",
+    reseñas:6,
+    pais:"USA",
+    titulo:"Lecciones de vida con Katie Zaferes",
+    precio:136,
+    grupo:false,
+    },
+  {
+    imagen:"/Assets/wedding-photography 1.png",
+    estado:"Disponible",
+    reseñas:3,
+    pais:"USA",
+    titulo:"Fotografia para bodas",
+    precio:250,
+    grupo:false,
+  },
+  {
+    imagen:"/Assets/mountain-bike 1.png",
+    estado:"Vendido",
+    reseñas:5,
+    pais:"USA",
+    titulo:"Ciclo montañismo en grupo",
+    precio:150,
+    grupo:true,
+  },
+  {
+    imagen:"/Assets/wedding-photography 1.png",
+    estado:"Disponible",
+    reseñas:3,
+    pais:"USA",
+    titulo:"Fotografia para bodas",
+    precio:250,
+    grupo:false,
+  }
+]
--- src/data/jokesData.js
+++
@@ -1,8 +0,0 @@
-export default [
-  {
-    setup: "I got my daughter a fridge for her birthday",
-    punch: "i can't wait to see he face light up when she opens it",
-  },
-  { setup: "How the hacker escape the police", punch: "He just ransomware!" },
-  { setup: "why don't pirates travel the mountains roads", punch: "Scurvy" },
-];
