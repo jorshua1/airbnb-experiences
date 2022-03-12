1049dc24850ab418417fef954355db42c81322e8
Jorshua Hernandez <jorshua321@gmail.com>
2022-03-11 23:58:22 -0500

    se sube actualizacion del componente de bromas en la rama props

--- src/App.js
+++ src/App.js
@@ -6,32 +6,11 @@ import Joke from "./Components/Joke.js";
 import jokesData from "./data/jokesData.js";
 
 function App() {
+  const jokesElements = jokesData.map((joke) => {
+    return <Joke key={joke.id} setup={joke.setup} punchline={joke.punchline} />;
+  });
 
-  const jokesElements = jokesData.map(joke =>{
-    return <Joke setup={joke.setup} punch={joke.punch} />
-  })
-
-  return (
-    <div className="">
-      {/* <Container />
-      <Joke
-        setup="I got my daughter a fridge for her birthday"
-        punch="i can't wait to see he face light up when she opens it"
-      />
-      <Joke
-        setup="How the hacker escape the police"
-        punch="He just ransomware!"
-      />
-      <Joke
-        setup="why don't pirates travel the mountains roads"
-        punch="Scurvy"
-      /> */}
-      {jokesElements}
-      {/* <Navbar /> */}
-      {/* <Hero /> */}
-      {/* <Cards /> */}
-    </div>
-  );
+  return <div className="">{jokesElements}</div>;
 }
 
 export default App;
--- src/Components/Joke.js
+++ src/Components/Joke.js
@@ -1,17 +1,19 @@
 import React from "react";
 
 function Joke(props) {
-  const colors = [
-    <h3>Red</h3>,
-    <h3>Blue</h3>,
-    <h3>White</h3>,
-    <h3>Green</h3>,
-    <h3>Black</h3>,
-  ];
+  const [isShown, setIsShown] = React.useState(false);
+  function toggleShown() {
+    // estado actual false, luego de ejecutada la funcion se cambia el estado al contrario del actual (false -> true)
+    setIsShown((prevShown) => !prevShown);
+    console.log(isShown)
+  }
+
   return (
     <div className="text-2xl m-4 p-4 bg-zinc-900 shadow-xl text-white">
-      <p>{props.setup}, {props.punch}</p>
-      {/* {colors} */}
+      {props.setup && <h3>{props.setup}</h3>}
+      {isShown && <p>{props.punchline}</p> }
+      {/* podemos agregar condicionales dentro del string de la etiqueta para editar el contenido de esta dicha */}
+      <button onClick={toggleShown} className="bg-white text-zinc-900 mt-6 p-3 rounded-lg">{isShown ? "Banish the " : "let me see the"} punchline</button>
     </div>
   );
 }
