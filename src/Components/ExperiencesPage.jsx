fbdb9366d8c16265841de76b875658f7ef8de7a3
Jorshua Hernandez <jorshua321@gmail.com>
2023-10-21 02:52:36 -0500

    Localizando componentes en una sola vista(pagina)

---
+++ src/Components/ExperiencesPage.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar.js";
+import Hero from "./Hero";
+import Cards from "./Cards";
+
+export function ExperiencesPage() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Cards />
+    </>
+  );
+}
